0b2c9e536bb1b8eec88af664291bd9c641a55df0
JuanDavid <jdgarzon64@misena.edu.co>
2018-03-21 21:18:35 -0500

    correction

--- app/src/main/java/com/example/papay/tallerencuestas/InformacionLaboralTab.java
+++ app/src/main/java/com/example/papay/tallerencuestas/InformacionLaboralTab.java
@@ -1,16 +1,29 @@
 package com.example.papay.tallerencuestas;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.io.File;
 
 
 public class InformacionLaboralTab extends Fragment {
-
+    EditText txtNombreEmpresaInfoLaboral;
+    View view;
+    ImageView imvFotoEmpresaInfoLaboral;
+    Button btnFotoInfoLaboral;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -22,7 +35,43 @@ public class InformacionLaboralTab extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_informacion_laboral_tab, container, false);
+        view = inflater.inflate(R.layout.fragment_informacion_laboral_tab, container, false);
+        txtNombreEmpresaInfoLaboral = view.findViewById(R.id.txtNombreEmpresaInfoLaboral);
+        imvFotoEmpresaInfoLaboral = view.findViewById(R.id.imvFotoEmpresaInfoLaboral);
+        imvFotoEmpresaInfoLaboral.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                recuperarFoto(getView());
+            }
+        });
+        btnFotoInfoLaboral = view.findViewById(R.id.btnFotoInfoLaboral);
+        btnFotoInfoLaboral.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tomarFoto(getView());
+                recuperarFoto(getView());
+            }
+        });
+
+
+        return view;
     }
 
+    public void tomarFoto(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        String filePath = Environment.getExternalStorageDirectory() + txtNombreEmpresaInfoLaboral.getText().toString() + ".jpg";
+        File foto = new File(getActivity().getExternalFilesDir(null), txtNombreEmpresaInfoLaboral.getText().toString() + ".jpg");
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(foto));
+        startActivity(intent);
+        String dir = getActivity().getExternalFilesDir(null) + "/" + txtNombreEmpresaInfoLaboral.getText().toString() + ".jpg";
+
+        startActivity(intent);
+
+    }
+
+    public void recuperarFoto(View view) {
+
+        Bitmap bitmap1 = BitmapFactory.decodeFile(getActivity().getExternalFilesDir(null) + "/" + txtNombreEmpresaInfoLaboral.getText().toString() + ".jpg");
+        imvFotoEmpresaInfoLaboral.setImageBitmap(bitmap1);
+    }
 }
