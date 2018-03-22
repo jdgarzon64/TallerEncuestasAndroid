a7e737d206f668fcb242fc0406f603c2e3112322
JuanDavid <jdgarzon64@misena.edu.co>
2018-03-22 08:53:47 -0500

    add familiar

---
+++ app/src/main/java/modelo/Familiar.java
@@ -0,0 +1,41 @@
+package modelo;
+
+/**
+ * Created by papay on 22/03/2018.
+ */
+
+public class Familiar {
+    public String nombre;
+    public String fechaDeNacimiento;
+    public String parentesco;
+
+    public Familiar(String nombre, String parentesco, String fechaDeNacimiento) {
+        this.nombre = nombre;
+        this.parentesco = parentesco;
+        this.fechaDeNacimiento = fechaDeNacimiento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getFechaDeNacimiento() {
+        return fechaDeNacimiento;
+    }
+
+    public void setFechaDeNacimiento(String fechaDeNacimiento) {
+        this.fechaDeNacimiento = fechaDeNacimiento;
+    }
+
+    public String getParentesco() {
+        return parentesco;
+    }
+
+    public void setParentesco(String parentesco) {
+        this.parentesco = parentesco;
+    }
+}
--- app/src/main/java/modelo/NucleoFamiliar.java
+++ app/src/main/java/modelo/NucleoFamiliar.java
@@ -7,47 +7,12 @@ import java.util.ArrayList;
  */
 
 public class NucleoFamiliar {
-    public String nombre;
-    public String fechaDeNacimiento;
-    public String parentesco;
-    public ArrayList<String>listaFamiliares;
 
-    public NucleoFamiliar(String nombre, String fechaDeNacimiento, String parentesco) {
-        this.nombre = nombre;
-        this.fechaDeNacimiento = fechaDeNacimiento;
-        this.parentesco = parentesco;
+    public ArrayList<Familiar>listaFamiliares;
+
+    public NucleoFamiliar() {
+
         this.listaFamiliares=new ArrayList<>();
     }
 
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getFechaDeNacimiento() {
-        return fechaDeNacimiento;
-    }
-
-    public void setFechaDeNacimiento(String fechaDeNacimiento) {
-        this.fechaDeNacimiento = fechaDeNacimiento;
-    }
-
-    public String getParentesco() {
-        return parentesco;
-    }
-
-    public void setParentesco(String parentesco) {
-        this.parentesco = parentesco;
-    }
-
-    public ArrayList<String> getListaFamiliares() {
-        return listaFamiliares;
-    }
-
-    public void setListaFamiliares(ArrayList<String> listaFamiliares) {
-        this.listaFamiliares = listaFamiliares;
-    }
 }
